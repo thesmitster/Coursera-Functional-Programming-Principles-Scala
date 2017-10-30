6ed49997b61465fdf6e87a3ff41e0951d4e1f105
Scott Smith <scott.smith.2@gmail.com>
2017-10-30 10:26:00 -0400

    Finished assignment

--- assignments/week-1-practice-assignment/src/main/scala/example/Lists.scala
+++ assignments/week-1-practice-assignment/src/main/scala/example/Lists.scala
@@ -1,5 +1,5 @@
 package example
-
+import java.util.NoSuchElementException
 
 object Lists {
 
@@ -23,8 +23,11 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = ???
-  
+    def sum(xs: List[Int]): Int = {
+      if (xs.isEmpty) 0
+      else xs.head + sum(xs.tail)
+    }
+
   /**
    * This method returns the largest element in a list of integers. If the
    * list `xs` is empty it throws a `java.util.NoSuchElementException`.
@@ -38,5 +41,16 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-    def max(xs: List[Int]): Int = ???
+    def max(xs: List[Int]): Int = {
+      if (xs.isEmpty)
+        throw new java.util.NoSuchElementException("Must input a list with elements")
+
+      try {
+        val rem = max(xs.tail)
+        if (xs.head > rem) xs.head
+        else rem
+      } catch {
+        case e:java.util.NoSuchElementException => xs.head
+      }
+    }
   }
--- assignments/week-1-practice-assignment/src/test/scala/example/ListsSuite.scala
+++ assignments/week-1-practice-assignment/src/test/scala/example/ListsSuite.scala
@@ -46,8 +46,8 @@ import org.scalatest.junit.JUnitRunner
    *
    * This allows tests to be written in a more readable manner:
    */
-  test("one plus one is three?") {
-    assert(1 + 1 == 3) // This assertion fails! Go ahead and fix it.
+  test("one plus two is three") {
+    assert(1 + 2 == 3) // This assertion fails! Go ahead and fix it.
   }
 
 
@@ -71,8 +71,8 @@ import org.scalatest.junit.JUnitRunner
    *
    * We recommend to always use the `===` equality operator when writing tests.
    */
-  test("details why one plus one is not three") {
-    assert(1 + 1 === 3) // Fix me, please!
+  test("details why one plus two is three") {
+    assert(1 + 2 === 3) // Fix me, please!
   }
 
   /**
@@ -100,6 +100,7 @@ import org.scalatest.junit.JUnitRunner
    * `List` object.
    */
   import Lists._
+  import java.util.NoSuchElementException
 
 
   /**
@@ -117,10 +118,45 @@ import org.scalatest.junit.JUnitRunner
     assert(sum(List(1,2,0)) === 3)
   }
 
+  test("sum of a single number") {
+    assert(sum(List(1)) === 1)
+  }
+
+  test("sum negative numbers") {
+    assert(sum(List(-1,-2,-3)) === -6)
+  }
+
+  test("sum empty list equals 0") {
+    assert(sum(List()) === 0)
+  }
+
+  test("sum repeated numbers") {
+    assert(sum(List(1,1,1,1,1,1,1,1,1,1)) == 10)
+  }
+
   test("max of a few numbers") {
     assert(max(List(3, 7, 2)) === 7)
   }
 
+  test("max of a single number") {
+    assert(max(List(1)) === 1)
+  }
 
+  test("max equals zero") {
+    assert(max(List(-1,0)) === 0)
+  }
 
+  test("max negative numbers") {
+    assert(max(List(-1,-2,-3)) === -1)
+  }
+
+  test("max throws NoSuchElementException when empty") {
+    intercept[java.util.NoSuchElementException] {
+      max(List())
+    }
+  }
+
+  test("max repeated numbers") {
+    assert(max(List(1,1,1,1,2,2,2,2)) === 2)
+  }
 }
